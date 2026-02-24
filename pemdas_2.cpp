77797938cefd128f6ca296d0a9f77a6fa8a99762
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-02-24 16:20:44 +0700

    commit 2

---
+++ pemdas_2.cpp
@@ -0,0 +1,19 @@
+//deklarassi library
+#include <iostream>
+using namespace std;
+
+//program utama
+int main()
+{
+    //deklarasi variabel umur
+    int umur;
+    //memberi nilai dari variabel
+    umur = 18;
+
+    //menampilkan isi string
+    cout <<"Paduka Rangga" << endl ;
+    //menampilkan isi string
+    cout << 911 <<endl;
+    //menampilkan isi integer
+    cout << umur <<endl;
+}
