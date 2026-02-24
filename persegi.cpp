77c70541ae168fc3ed27dce2e5ef64c00b0e7b3c
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-02-24 16:25:56 +0700

    commit 3

---
+++ persegi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{//start
+    float p,l,luas;
+
+//langkah 2 : masukan panjang 
+cout << "masukan panjang : " ;
+cin >> p;
+//langkah 3 : masukan lebar
+cout << "masukan lebar : " ;
+cin >> l;
+//langkah 4 : hitung luas = p x l
+luas = p*l;
+//langkah 5 :
+cout << "hasilnya = " << luas ;
+}//selesai
