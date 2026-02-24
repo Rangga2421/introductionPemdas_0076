6bd85677d2eafca56901ba132cb918feccf68fe4
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-02-24 16:27:20 +0700

    commit 4

---
+++ biodata.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 100.5;
+    double harga = 200000.00;
+    string nama = "rangganteng";
+
+    jk = 'l';
+    cout << "selamat datang" << endl;
+    cout << "di kerajaan\n";
+    cout << "atlantis\n";
+
+    nama = "rangganteng";
+    cout << "Nama Saya : " << nama;
+    
+}
